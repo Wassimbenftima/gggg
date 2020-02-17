bfde108f7651c08e9fa17ae38f6c89439cd79f7a
Wassimbenftima <wassimbenftima92@gmail.com>
2020-02-17 15:15:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Timer from "./components/timer";
+import "./App.css";
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Timer/>
+    </div>
+  );
+}
+
+export default App;
